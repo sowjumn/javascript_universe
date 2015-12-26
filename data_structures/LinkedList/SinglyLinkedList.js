f1aff04797cfcfe05b377a0a5b8cd6ce0df776d7
sowjanya <sowju19@gmail.com>
2015-12-25 23:17:30 -0800

    some more challenges

--- data_structures/LinkedList/SinglyLinkedList.js
+++ data_structures/LinkedList/SinglyLinkedList.js
@@ -13,14 +13,29 @@ SinglyLinkedList.prototype.addNodeToBegin = function(val) {
 SinglyLinkedList.prototype.printList = function() {
   var nextPtr = this.listHead;
   while (nextPtr) {
-    console.log(nextPtr.value);
+    console.log(nextPtr.value + '->');
     nextPtr = nextPtr.next;
   }
 }
 
+SinglyLinkedList.prototype.findMiddleElement = function() {
+  var nextPtr = this.listHead;
+  var doubleNextPtr = this.listHead;
+  while (doubleNextPtr.next) {
+    nextPtr = nextPtr.next;
+    doubleNextPtr = doubleNextPtr.next.next;
+  }
+  console.log(nextPtr.value);
+}
+
 var myAwesomeList = new SinglyLinkedList();
-myAwesomeList.addNodeToBegin('S');
-myAwesomeList.addNodeToBegin('M');
-myAwesomeList.addNodeToBegin('N');
+myAwesomeList.addNodeToBegin('7');
+myAwesomeList.addNodeToBegin('8');
+myAwesomeList.addNodeToBegin('4');
+myAwesomeList.addNodeToBegin('3');
+myAwesomeList.addNodeToBegin('5');
+myAwesomeList.addNodeToBegin('9');
+myAwesomeList.addNodeToBegin('10');
 
 myAwesomeList.printList();
+myAwesomeList.findMiddleElement();
